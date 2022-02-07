1b8f8a8d2fbe587b6d21a72b862cc1ef06c27c9e
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-02-07 22:11:13 +0200

    oop method overloading sample

---
+++ OopMethods/OopMethods/ConsoleHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopMethods
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintMessage(string message)
+        {
+            PrintMessage(message, null, 1);
+        }
+
+        public static void PrintMessage(string message, string label)
+        {
+            PrintMessage(message, label, 1);
+        }
+
+        public static void PrintMessage(string message, uint count)
+        {
+            PrintMessage(message, null, count);
+        }
+
+        public static void PrintMessage(string message, string label, uint count)
+        {
+            if (!string.IsNullOrEmpty(label))
+            {
+                Console.Write($"{label}:");
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ OopMethods/OopMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OopMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.PrintMessage("Text", "Hello World");
+        }
+    }
+}
